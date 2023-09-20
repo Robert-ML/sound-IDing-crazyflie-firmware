d99325c0743aeeb02aeb9f177e75447f205e0bc0
Robert-ML <37192540+Robert-ML@users.noreply.github.com>
2023-09-20 13:37:58 +0200

    motors send different messages

--- src/modules/src/motor_chirp.c
+++ src/modules/src/motor_chirp.c
@@ -32,112 +32,134 @@ static struct {
   uint16_t dF;
   uint8_t message;
   uint8_t msgCounter;
-} _msgParams;
+} _msgParams[4];
 
-MotorChirpParameters _chirpParams = {16000, 500, 2000};
+MotorChirpParameters _chirpParams[4] = {{13000, 500, 2000}, {15000, 500, 2000}, {13000, 500, 2000}, {15000, 500, 2000}};
 
 static bool isInit=false;
-static uint8_t message = 0xaa;
+static uint8_t message[4] = {0xaa, 0xaa, 0xaa, 0xaa};
 
 static bool requestChirp = false;
-static uint16_t startFreq = 0;
-static uint16_t endFreq = 0;
-static int16_t fStep = 0;
-static uint16_t lastFreq = 0;
-static uint16_t motorFreq = 0;
+static uint16_t startFreq[4] = {0, 0, 0, 0};
+static uint16_t endFreq[4]   = {0, 0, 0, 0};
+static int16_t fStep[4]      = {0, 0, 0, 0};
+static uint16_t lastFreq[4]  = {0, 0, 0, 0};
+static uint16_t motorFreq[4] = {0, 0, 0, 0};
 
-static bool inPause = false;
-static uint8_t pauseTicks = 0;
+static bool inPause[4] = {false, false, false, false};
+static uint8_t pauseTicks[4] = {0, 0, 0, 0};
 
 #define SND_TASK_INTERVAL 50
 
-static bool doingMsg = false;
+static bool doingMsg[4] = {false, false, false, false};
 
 static xTimerHandle timer;
 
-static void setupNextChirp()
+static void updateChirpFreq(const uint32_t id);
+
+static void setupNextChirp(const uint32_t id)
 {
   // we don't check msgCounter here........
-  pauseTicks = 0;
-  bool isDownChirp = !(_msgParams.message & (1 << _msgParams.msgCounter));
+  pauseTicks[id] = 0;
+  bool isDownChirp = !(_msgParams[id].message & (1 << _msgParams[id].msgCounter));
   if (isDownChirp){
-    startFreq = _msgParams.topF;
-    endFreq = _msgParams.bottomF;
-    fStep = -_msgParams.dF;
+    startFreq[id] = _msgParams[id].topF;
+    endFreq[id] = _msgParams[id].bottomF;
+    fStep[id] = -_msgParams[id].dF;
   } else {
     // upchirp
-    startFreq = _msgParams.bottomF;
-    endFreq = _msgParams.topF;
-    fStep = _msgParams.dF;
+    startFreq[id] = _msgParams[id].bottomF;
+    endFreq[id] = _msgParams[id].topF;
+    fStep[id] = _msgParams[id].dF;
   }
-  motorFreq = startFreq;
-  _msgParams.msgCounter += 1;
+  motorFreq[id] = startFreq[id];
+  _msgParams[id].msgCounter += 1;
 }
 
-static void setupMessage()
+static void setupMessage(const uint32_t id)
 {
-  int16_t totalFChange = (int16_t)((float)_chirpParams.chirpSlope * _chirpParams.chirpLen / 1000.0f);
-  _msgParams.dF = (int16_t)((float)_chirpParams.chirpSlope * SND_TASK_INTERVAL / 1000.0f);
-  _msgParams.bottomF = _chirpParams.centerFreq - totalFChange/2;
-  _msgParams.topF = _chirpParams.centerFreq + totalFChange/2;
-  _msgParams.message = message;
-  _msgParams.msgCounter = 0;
-  inPause = false;
+  int16_t totalFChange = (int16_t)((float)_chirpParams[id].chirpSlope * _chirpParams[id].chirpLen / 1000.0f);
+  _msgParams[id].dF = (int16_t)((float)_chirpParams[id].chirpSlope * SND_TASK_INTERVAL / 1000.0f);
+  _msgParams[id].bottomF = _chirpParams[id].centerFreq - totalFChange/2;
+  _msgParams[id].topF = _chirpParams[id].centerFreq + totalFChange/2;
+  _msgParams[id].message = message[id];
+  _msgParams[id].msgCounter = 0;
+  inPause[id] = false;
 }
 
-static void updateChirpFreq()
- {
+static void updateAllChirpsFreq()
+{
+  uint32_t i;
 
-  if (!doingMsg && requestChirp) {
-    doingMsg = true;
-    requestChirp = false;
+  for (i = 0; i < NBR_OF_MOTORS; ++i)
+  {
+    updateChirpFreq(i);
+  }
+}
 
-    setupMessage();
-    setupNextChirp();
+static void updateChirpFreq(const uint32_t id)
+{
+
+  if (!doingMsg[id] && requestChirp)
+  {
+    doingMsg[id] = true;
+    if (id == 3)
+    {
+      requestChirp = false;
+    }
+
+    setupMessage(id);
+    setupNextChirp(id);
   }
 
-  if (motorFreq != lastFreq) {
-#if ALL_MOTOR_CHIRP
-#pragma message "Using all motors to chirp"
-    for (int ii=0; ii<4; ii++) motorsSetFrequency(ii,motorFreq);
-#else
-#pragma message "Using M4 only for chirp"
-      motorsSetFrequency(3, motorFreq);
-#endif
-      lastFreq = motorFreq;
-  }
+  if (motorFreq[id] != lastFreq[id])
+  {
+    motorsSetFrequency(id, motorFreq[id]);
+    lastFreq[id] = motorFreq[id];
+}
 
-  if (doingMsg) {
-    if (inPause) {
-      pauseTicks += 1;
-      if (pauseTicks > PAUSE_LENGTH) {
-        inPause = false;
-	      setupNextChirp();
-      } else {
+  if (doingMsg[id])
+  {
+    if (inPause[id])
+    {
+      pauseTicks[id] += 1;
+      if (pauseTicks[id] > PAUSE_LENGTH)
+      {
+        inPause[id] = false;
+	      setupNextChirp(id);
+      }
+      else
+      {
         return; // don't do anything if we were pausing
       }
     }
     // we were not pausing, see if we're done with this symbol
-    bool finished = (fStep < 0 && motorFreq < endFreq) || (fStep > 0 && motorFreq > endFreq);
-    if (finished) { // symbol done
-      motorFreq = 0;
-      if (_msgParams.msgCounter >= MSG_LENGTH) {
+    bool finished = (fStep[id] < 0 && motorFreq[id] < endFreq[id]) || (fStep[id] > 0 && motorFreq[id] > endFreq[id]);
+    if (finished)
+    { // symbol done
+      motorFreq[id] = 0;
+      if (_msgParams[id].msgCounter >= MSG_LENGTH)
+      {
         // we have finished all the symbols in the message
-        doingMsg = false;
-      } else {
-        // we just have to wait a few ticks to the next symbol
-        inPause = true;
+        doingMsg[id] = false;
       }
-    } else {
+      else
+      {
+        // we just have to wait a few ticks to the next symbol
+        inPause[id] = true;
+      }
+    }
+    else
+    {
       // we weren't done with the symbol
-      motorFreq += fStep;
+      motorFreq[id] += fStep[id];
     }
   }
- }
+}
 
 static void motorSoundTimer(xTimerHandle timer)
 {
-  workerSchedule(updateChirpFreq, NULL);
+  workerSchedule(updateAllChirpsFreq, NULL);
 }
 
 void motorSoundInit(void)
@@ -158,14 +180,18 @@ bool motorChirpTest(void)
 
 const MotorChirpParameters *currentMotorParams()
 {
-  return &_chirpParams;
+  return &(_chirpParams[0]);
 }
 PARAM_GROUP_START(chirp)
-PARAM_ADD(PARAM_UINT16, length, &(_chirpParams.chirpLen))
-PARAM_ADD(PARAM_UINT16, center, &(_chirpParams.centerFreq))
-PARAM_ADD(PARAM_INT16, slope, &(_chirpParams.chirpSlope))
-PARAM_ADD(PARAM_UINT8, message, &message)
-PARAM_ADD(PARAM_UINT8, goChirp, &requestChirp)
+PARAM_ADD(PARAM_UINT16, center_0, &(_chirpParams[0].centerFreq))
+PARAM_ADD(PARAM_UINT16, center_1, &(_chirpParams[1].centerFreq))
+PARAM_ADD(PARAM_UINT16, center_2, &(_chirpParams[2].centerFreq))
+PARAM_ADD(PARAM_UINT16, center_3, &(_chirpParams[3].centerFreq))
+PARAM_ADD(PARAM_UINT8, message_1, &(message[0]))
+PARAM_ADD(PARAM_UINT8, message_2, &(message[1]))
+PARAM_ADD(PARAM_UINT8, message_3, &(message[2]))
+PARAM_ADD(PARAM_UINT8, message_4, &(message[3]))
+PARAM_ADD(PARAM_UINT8, goChirp,   &requestChirp)
 PARAM_GROUP_STOP(chirp)
 
 LOG_GROUP_START(chirp)
