df600a4b84c4a56357b9ce214e469a62c444c2a9
Robert-ML <37192540+Robert-ML@users.noreply.github.com>
2023-11-24 09:49:31 +0100

    moore motors can transmit and custom longer message

--- src/modules/src/motor_transmission.c
+++ src/modules/src/motor_transmission.c
@@ -18,9 +18,9 @@
 // Defines
 #define TESTING_MODULE
 
-#define CENTER_FREQ 17000
+#define CENTER_FREQ 24000
 #define BANDWIDTH 1000
-#define SYMBOL_LENGTH 1000
+#define SYMBOL_LENGTH 100
 // #define SYMBOL_PAUSE 100
 #define SYMBOL_PAUSE_COMPENSATION 50 // the mottors continue to resonate 50 ms
 #define SIMPLIFIED_MODULATION false
@@ -43,8 +43,11 @@ static uint16_t center_freq       = CENTER_FREQ;
 static uint16_t bandwidth         = BANDWIDTH;
 static uint16_t symbol_length     = SYMBOL_LENGTH;
 // static uint16_t symbol_pause      = SYMBOL_PAUSE;
-static bool simplifyed_modulation = SIMPLIFIED_MODULATION;
 static bool update_module_params  = false;
+
+static bool use_m123 = false;
+static bool use_m4   = true;
+
 #endif
 
 // Funciton prototypes
@@ -109,6 +112,11 @@ static void periodic_task(xTimerHandle timer)
     workerSchedule(update_transmission, NULL);
 }
 
+// static uint8_t stream[] = {0x00, 0x55, 0x45, 0x0C, 0xA9, 0xFF};
+static uint8_t stream[] = {0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x21};
+static uint8_t index = 0xFF;
+static bool transmitting_stream = false;
+
 static void update_transmission()
 {
 #ifdef TESTING_MODULE
@@ -117,18 +125,37 @@ static void update_transmission()
 
     mod_transmit(&transmission_m4);
 
-    if (transmit && mod_is_transmitting(&transmission_m4) == false) {
-        // transmit = 0;
-        error_code = mod_prepare_and_transmit(&transmission_m4, message);
+    if (transmit && !transmitting_stream) {
+        transmit = 0;
+        transmitting_stream = true;
     }
 
+    if (transmitting_stream && mod_is_transmitting(&transmission_m4) == false) {
+        index += 1;
+
+        if (index >= (sizeof(stream) / sizeof(stream[0]))) {
+            index = 0xFF;
+            transmitting_stream = false;
+        } else {
+            error_code = mod_prepare_and_transmit(&transmission_m4, stream[index]);
+        }
+    }
 }
 
 static void transmit_motor4(const uint32_t frequency)
 {
     static uint32_t last_frequency = 0;
     if (last_frequency != frequency) {
-        motorsSetFrequency(MOTOR_M4, (uint16_t)frequency);
+        if (use_m4) {
+            motorsSetFrequency(MOTOR_M4, (uint16_t)frequency);
+        }
+
+        if (use_m123) {
+            motorsSetFrequency(MOTOR_M1, (uint16_t)frequency);
+            motorsSetFrequency(MOTOR_M2, (uint16_t)frequency);
+            motorsSetFrequency(MOTOR_M3, (uint16_t)frequency);
+        }
+
         last_frequency = frequency;
     }
 }
@@ -163,9 +190,9 @@ PARAM_ADD(PARAM_UINT8, Message, &message)
 PARAM_ADD(PARAM_UINT16, center_freq,   &center_freq)
 PARAM_ADD(PARAM_UINT16, bandwidth,     &bandwidth)
 PARAM_ADD(PARAM_UINT16, symbol_length, &symbol_length)
-// PARAM_ADD(PARAM_UINT16, symbol_pause,  &symbol_pause)
-PARAM_ADD(PARAM_UINT8, simple_mod, &simplifyed_modulation)
 PARAM_ADD(PARAM_UINT8, update_params,  &update_module_params)
+PARAM_ADD(PARAM_UINT8, use_m123, &use_m123)
+PARAM_ADD(PARAM_UINT8, use_m4,   &use_m4)
 #endif
 
 PARAM_GROUP_STOP(m_comms)
